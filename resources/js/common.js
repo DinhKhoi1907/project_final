22d315a058ece7b26565e334403d34898e5d3bec
DinhKhoi1907 <mkgketban123@gmail.com>
2021-05-04 11:32:07 +0700

    axios and mixin

---
+++ resources/js/common.js
@@ -0,0 +1,52 @@
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        async callApi(method, url, dataObj) {
+            try {
+                // Send a POST request
+                return await axios({
+                    method: method,
+                    url: url,
+                    data: dataObj,
+                });
+            } catch (e) {
+                return e.response
+            }
+        },
+
+        i(desc, title = "Hey") {
+            this.$Notice.info({
+                title: title,
+                desc: desc
+            });
+        },
+        s(desc, title = "Greate!") {
+            this.$Notice.success({
+                title: title,
+                desc: desc
+            });
+        },
+        w(desc, title = "Oops!") {
+            this.$Notice.warning({
+                title: title,
+                desc: desc
+            });
+        },
+        e(desc, title = "Oops!") {
+            this.$Notice.error({
+                title: title,
+                desc: desc
+            });
+        },
+        swr(desc = 'Somethign want wrong! Please try again', title = "Oops!") {
+            this.$Notice.error({
+                title: title,
+                desc: desc
+            });
+        }
+    }
+}
